7d4c3cf438178886eafd2627c5b2fc30a112285a
jeff1evesque <jeff1evesque@yahoo.com>
2019-04-19 11:53:52 +0000

    #1: config.py, add file

---
+++ config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+'''
+
+configurations and credentials for various endpoints.
+
+'''
+
+twitter_api = {[
+    'CONSUMER_KEY' = 'REPLACE-ME',
+    'CONSUMER_SECRET' = 'REPLACE-ME',
+    'ACCESS_TOKEN' = 'REPLACE-ME',
+    'ACCESS_SECRET' = 'REPLACE-ME'
+]}
