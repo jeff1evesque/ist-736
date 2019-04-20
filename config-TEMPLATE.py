49f6da9d2fb7e8bd18bebefcb4b4df4e86d1f80a
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-20 13:35:37 -0400

    #1: config-TEMPLATE.py, fix typo

--- config-TEMPLATE.py
+++ config-TEMPLATE.py
@@ -7,9 +7,9 @@ TEMPLATE: copy this file as 'config.py', then change any of the below
 
 '''
 
-twitter_api = {[
-    'CONSUMER_KEY' = 'REPLACE-ME',
-    'CONSUMER_SECRET' = 'REPLACE-ME',
-    'ACCESS_TOKEN' = 'REPLACE-ME',
-    'ACCESS_SECRET' = 'REPLACE-ME'
-]}
+twitter_api = {
+    'CONSUMER_KEY': 'REPLACE-ME',
+    'CONSUMER_SECRET': 'REPLACE-ME',
+    'ACCESS_TOKEN': 'REPLACE-ME',
+    'ACCESS_SECRET': 'REPLACE-ME'
+}
