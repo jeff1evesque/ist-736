531503e0855a72b2523e5f1908e97dc167323286
jeffrey levesque <jeff1evesque@yahoo.com>
2019-05-25 09:48:56 -0400

    #44: timeseries, add initial model file

---
+++ model/timeseries.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import re
+from pathlib import Path
+from algorithm.arima import Arima
+from algorithm.lstm import Lstm
+
+def model(
+    df,
+    normalize_key,
+    model_type='arima',
+):
+    '''
+
+    return trained classifier.
+
+    '''
+
+    # initialize classifier
+    if model_type == 'arima':
+        model = Arima(df=df, normalize_key=normalize_key)
+
+        # train: use rolling length
+        iterations = len(model.get_data(key=normalize_key)[1])
+        model.train(iterations)
+
+    elif model_type == 'lstm':
+        model = Lstm(df=df, normalize_key=normalize_key)
+        model.train()
+
+    return(model)
