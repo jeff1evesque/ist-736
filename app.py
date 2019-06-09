55473e512cbf8144f2c8c6f2d404fc93f4bc50aa
jeffrey levesque <jeff1evesque@yahoo.com>
2019-06-09 12:21:03 -0400

    #60: implement hw8 exploratory

--- app.py
+++ app.py
@@ -115,6 +115,9 @@ for i,sn in enumerate(screen_name):
     # convert to string
     data[sn]['created_at'] = data[sn]['created_at'].astype(str)
 
+    # exploratory: wordcloud + sentiment
+    explore(data[sn], stopwords=stopwords)
+
     # largest time span
     start = data[screen_name[i]]['created_at'].iloc[0]
     temp_start = datetime.strptime(start.split()[0], '%Y-%m-%d')
@@ -125,6 +128,12 @@ for i,sn in enumerate(screen_name):
     temp_end = datetime.strptime(end.split()[0], '%Y-%m-%d')
     if temp_end > end_date:
         end_date = temp_end
+
+#
+# exploratory (overall): wordcloud + sentiment
+#
+df = pd.concat(data).reset_index()
+explore(df, stopwords=stopwords, sent_cases={'screen_name': screen_name})
 
 #
 # timeseries analysis: sentiment
@@ -316,9 +325,3 @@ plot_bar(
     filename='mse_overall_lstm.png',
     rotation=90
 )
-
-#
-# exploratory
-#
-df = pd.concat(data).reset_index()
-explore(df, stopwords=stopwords, sent_cases={'screen_name': screen_name})
--- view/exploratory.py
+++ view/exploratory.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import re
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,47 +8,127 @@ from exploratory.sentiment import Sentiment
 from exploratory.word_cloud import word_cloud
 from utility.dataframe import cleanse
 
-def explore(df, sent_cases, stopwords='', target='full_text', rotation=0):
+def explore(
+    df,
+    sent_cases=None,
+    stopwords=[],
+    target='full_text',
+    background_color='white',
+    directory='viz',
+    suffix='',
+    plot_wc=True,
+    plot_sentiment=True,
+    plot_wc_overall=True,
+    plot_sentiment_overall=True,
+    cleanse=True
+):
     '''
 
     generate wordclouds and sentiment series plot.
 
     @target, dataframe column to parse
-    @sent_cases, dict where keys represent columns, and
-        values represent list of possible column values.
+    @sent_cases, dict where keys represent columns, and values represent list
+        of possible column values. This is used to filter the dataframe.
 
     '''
 
-    cases = []
-    for k,val in sent_cases.items():
-        for v in val:
-            # load select data
-            wc_temp = df.loc[df[k] == v]
+    if cleanse:
+        df[target] = cleanse(df, target, ascii=True)
+    else:
+        df[target] = [re.sub(
+            "'",
+            '',
+            str(s)
+        ) for s in df[target]]
 
+    if sent_cases:
+        for k,val in sent_cases.items():
+            for v in val:
+                if not os.path.exists('{d}/{value}'.format(
+                    d=directory,
+                    value=v
+                )):
+                    os.makedirs('{d}/{value}'.format(
+                        d=directory,
+                        value=v
+                    )
+
+                if plot_wc:
+                    wc_temp = df.loc[df[k] == v]
+
+                    # create wordcloud
+                    word_cloud(
+                        wc_temp,
+                        filename='{d}/{value}/wc{suffix}.png'.format(
+                            d=directory,
+                            value=v,
+                            suffix=suffix
+                        ),
+                        stopwords=stopwords,
+                        background_color=background_color
+                    )
+
+                if plot_sentiment:
+                    # create sentiment plot
+                    sent_temp = Sentiment(wc_temp, target)
+                    sent_temp.vader_analysis()
+                    sent_temp.plot_ts(
+                        title='{value}'.format(value=v),
+                        filename='{d}/{value}/sentiment{suffix}.png'.format(
+                            d=directory,
+                            value=v,
+                            suffix=suffix
+                        )
+                    )
+
+        if plot_wc_overall:
+            word_cloud(
+                df[target],
+                filename='{d}/wc_overall{suffix}.png'.format(
+                    d=directory,
+                    suffix=suffix
+                ),
+                stopwords=stopwords,
+                background_color=background_color
+            )
+
+        if plot_sentiment_overall:
+            sent_overall = Sentiment(df, target)
+            sent_overall.vader_analysis()
+            sent_overall.plot_ts(
+                title='Overall Sentiment',
+                filename='{d}/sentiment_overall{suffix}.png'.format(
+                    d=directory,
+                    suffix=suffix
+                )
+            )
+
+    else:
+        if plot_wc:
             # clean text
-            wc_temp[target] = cleanse(wc_temp, target, ascii=True)
+            wc_temp = df
+            wc_temp[target] = [' '.join(x) for x in wc_temp[target]]
+            wc_temp[target] = cleanse(df, target, ascii=True)
 
             # create wordcloud
             word_cloud(
-                wc_temp[target],
-                filename='viz/{value}/wc.png'.format(value=v, key=k),
-                stopwords=stopwords
+                wc_temp,
+                filename='{d}/wc{suffix}.png'.format(
+                    d=directory,
+                    suffix=suffix
+                ),
+                stopwords=stopwords,
+                background_color=background_color
             )
 
+        if plot_sentiment:
             # create sentiment plot
             sent_temp = Sentiment(wc_temp, target)
             sent_temp.vader_analysis()
             sent_temp.plot_ts(
-                title='{value}'.format(value=v),
-                filename='viz/{value}/sentiment.png'.format(value=v, key=k),
-                rotation=90
+                title='Sentiment Analysis',
+                filename='{d}/sentiment{suffix}.png'.format(
+                    d=directory,
+                    suffix=suffix
+                )
             )
-
-        word_cloud(df[target], filename='viz/wc_overall.png')
-        sent_overall = Sentiment(df, target)
-        sent_overall.vader_analysis()
-        sent_overall.plot_ts(
-            title='Overall Sentiment',
-            filename='viz/sentiment_overall.png',
-            rotation=90
-        )
