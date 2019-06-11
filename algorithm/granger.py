353b2b9eae4e234b4631d2f4c686616559067c9c
jeffrey levesque <jeff1evesque@yahoo.com>
2019-06-11 18:00:17 -0400

    #67: granger.py, adjust docstring

--- algorithm/granger.py
+++ algorithm/granger.py
@@ -6,7 +6,10 @@ from statsmodels.tsa.stattools import grangercausalitytests
 def granger(df, maxlag=1):
     '''
 
-    implement granger test for causality. null hypothesis 
+    implement granger test for causality.
+
+    null hypothesis: the series in the second column, does not granger cause
+        the series in the first column.
 
     @df, test time series in the second column Granger causes the time series
         in the first column.
