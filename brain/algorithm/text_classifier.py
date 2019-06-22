be83433f9fed957c09dec30f58cfcbb0a7259b53
jeffrey levesque <jeff1evesque@yahoo.com>
2019-06-22 17:47:17 -0400

    #85: text_classifier.py, fix chi2 order

--- brain/algorithm/text_classifier.py
+++ brain/algorithm/text_classifier.py
@@ -616,12 +616,11 @@ class Model():
         '''
 
         if self.wscores:
-            if type == 'bow':
-                x = self.bow
-            else:
-                x = self.tfidf
+            # remove nan
+            result = [x for x in self.wscores if not np.isnan(x[1])]
 
-            wchi2 = sorted(self.wscores, key=lambda x:x[1])
+            # generate labels and scores
+            wchi2 = sorted(result, key=lambda x:x[1])
             labels, scores = zip(*wchi2[-top_words:])
 
             return(labels, scores)
