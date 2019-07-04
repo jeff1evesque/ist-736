b89ef6800465837cfdc78dbc72615f02b5493fde
jeffrey levesque <jeff1evesque@yahoo.com>
2019-07-03 20:59:28 -0400

    #79: twitter.py, unrelated fix

--- consumer/twitter.py
+++ consumer/twitter.py
@@ -13,6 +13,7 @@ def tweet_sn(
     screen_name,
     start_date,
     end_date,
+    'full_text'='full_text',
     directory='data/twitter',
     count=600,
     rate_limit=900
@@ -44,8 +45,8 @@ def tweet_sn(
                     params=[
                         {'user': ['screen_name']},
                         'created_at',
-                        classify_index,
-                        {'retweeted_status': [classify_index]},
+                        'full_text',
+                        {'retweeted_status': ['full_text']},
                         'retweet_count',
                         'favorite_count',
                         {'entities': ['user_mentions']}
@@ -55,7 +56,7 @@ def tweet_sn(
                 )
 
                 # sentiment analysis
-                s = Sentiment(data[sn], classify_index)
+                s = Sentiment(data[sn], 'full_text')
                 data[sn] = pd.concat([s.vader_analysis(), data[sn]], axis=1)
                 data[sn].replace('\s+', ' ', regex=True, inplace=True)
 
