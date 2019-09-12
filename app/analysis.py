0cbd2c9a8549b6b5b7ade4192296e66ba925afa4
jeffrey levesque <jeff1evesque@yahoo.com>
2019-09-11 23:53:58 -0400

    #100: refactor app code

--- app/analysis.py
+++ app/analysis.py
@@ -10,7 +10,7 @@ from brain.exploratory.sentiment import Sentiment
 from brain.controller.classifier import classify
 from brain.controller.timeseries import Timeseries
 from brain.controller.granger import granger
-from brain.controller.peak_detection import peak_detection
+from app.join_data import join_data
 
 
 def analyze(
@@ -47,13 +47,6 @@ def analyze(
         os.makedirs(directory_report)
 
     for i,sn in enumerate(screen_name):
-        #
-        # rename column: prevent collision with 'reset_index'
-        #
-        if 'index' in data[sn]:
-            data[sn].rename(columns={'index': 'index_val'}, inplace=True)
-
-        # create directories
         if not os.path.exists('{directory}/{sn}/granger'.format(
             directory=directory,
             sn=sn
@@ -63,41 +56,86 @@ def analyze(
                 sn=sn
             ))
 
-        # consistent datetime
-        data[sn]['created_at'] = [datetime.strptime(
-            x.split()[0],
-            '%Y-%m-%d'
-        ) for x in data[sn]['created_at']]
+    #
+    # join data: twitter and quandl
+    #
+    joined_data = join_data(
+        data=data,
+        df_quandl=df_quandl,
+        screen_name=screen_name,
+        directory=directory,
+        sentiments=sentiments,
+        classify_index=classify_index,
+        ts_index=ts_index
+    )
 
-        # convert to string
-        data[sn]['created_at'] = data[sn]['created_at'].astype(str)
+    #
+    # granger analysis
+    #
+    if analysis_granger:
+        initialized_data = joined_data
 
-        #
-        # some screen_name text multiple times a day, yet quandl only provides
-        #     daily prices.
-        #
-        data[sn] = data[sn].groupby(g).agg({
-            classify_index: lambda a: ''.join(map(str, a))
-        }).reset_index()
+        for i,sn in enumerate(screen_name):
+            # merge with consistent date format
+            initialized_data[sn]['created_at'] = [datetime.strptime(
+                x.split()[0],
+                '%Y-%m-%d'
+            ) for x in initialized_data[sn]['created_at']]
 
-        #
-        # timeseries analysis: sentiment
-        #
-        if analysis_ts_sentiment:
+            # convert to string
+            initialized_data[sn]['created_at'] = initialized_data[sn]['created_at'].astype(str)
+
+            #
+            # some screen_name text multiple times a day, yet quandl only provides
+            #     daily prices.
+            #
+            initialized_data[sn] = initialized_data[sn].groupby(g).agg({
+                classify_index: lambda a: ''.join(map(str, a))
+            }).reset_index()
+
+            initialized_data[sn] = initialized_data[sn].set_index('created_at').join(
+                df_quandl.set_index('date'),
+                how='left'
+            ).reset_index()
+
+            #
+            # granger causality
+            #
+            # Note: this requires the above quandl join.
+            #
+            for sentiment in sentiments:
+                if all(x in initialized_data[sn] for x in [ts_index, sentiment]):
+                    granger(
+                        initialized_data[sn][[ts_index, sentiment]],
+                        maxlag=3,
+                        directory='{directory}/{sn}/granger'.format(
+                            directory=directory,
+                            sn=sn
+                        ),
+                        suffix=sentiment
+                    )
+
+    #
+    # timeseries analysis: sentiment
+    #
+    if analysis_ts_sentiment:
+        initialized_data = joined_data
+
+        for i,sn in enumerate(screen_name):
             #
             # sentiment scores
             #
-            s = Sentiment(data[sn], classify_index)
-            data[sn] = pd.concat([s.vader_analysis(), data[sn]], axis=1)
-            data[sn].replace('\s+', ' ', regex=True, inplace=True)
+            s = Sentiment(initialized_data[sn], classify_index)
+            initialized_data[sn] = pd.concat([s.vader_analysis(), initialized_data[sn]], axis=1)
+            initialized_data[sn].replace('\s+', ' ', regex=True, inplace=True)
 
             #
             # timeseries model on sentiment
             #
             for sentiment in sentiments:
-                if all(x in data[sn] for x in [sentiment, 'created_at']):
+                if all(x in initialized_data[sn] for x in [sentiment, 'created_at']):
                     ts_sentiment = Timeseries(
-                        df=data[sn],
+                        df=initialized_data[sn],
                         normalize_key=sentiment,
                         date_index='created_at',
                         directory='{directory}/{sn}'.format(
@@ -122,187 +160,38 @@ def analyze(
                                 file=fp
                             )
 
-    #
-    # plot sentiment scores
-    #
-    if analysis_ts_sentiment:
-        if any(
-            pd.notnull(k) and
-            pd.notnull(v) and
-            'arima' in v for k,v in ts_results_sentiment.items()
-        ):
-            plot_bar(
-                labels=[k for k,v in ts_results_sentiment.items() if 'arima' in v],
-                performance=[v['arima']['mse']
-                    for k,v in ts_results_sentiment.items() if 'arima' in v],
-                directory='{directory}'.format(directory=directory),
-                filename='mse_overall_arima_sentiment.png',
-                rotation=90
-            )
+            if any(
+                pd.notnull(k) and
+                pd.notnull(v) and
+                'arima' in v for k,v in ts_results_sentiment.items()
+            ):
+                plot_bar(
+                    labels=[k for k,v in ts_results_sentiment.items() if 'arima' in v],
+                    performance=[v['arima']['mse']
+                        for k,v in ts_results_sentiment.items() if 'arima' in v],
+                    directory='{directory}'.format(directory=directory),
+                    filename='mse_overall_arima_sentiment.png',
+                    rotation=90
+                )
 
-        if any(
-            pd.notnull(k) and
-            pd.notnull(v) and
-            'lstm' in v for k,v in ts_results_sentiment.items()
-        ):
-            plot_bar(
-                labels=[k for k,v in ts_results_sentiment.items() if 'lstm' in v],
-                performance=[v['lstm']['mse']
-                    for k,v in ts_results_sentiment.items() if 'lstm' in v],
-                directory='{directory}'.format(directory=directory),
-                filename='mse_overall_lstm_sentiment.png',
-                rotation=90
-            )
+            if any(
+                pd.notnull(k) and
+                pd.notnull(v) and
+                'lstm' in v for k,v in ts_results_sentiment.items()
+            ):
+                plot_bar(
+                    labels=[k for k,v in ts_results_sentiment.items() if 'lstm' in v],
+                    performance=[v['lstm']['mse']
+                        for k,v in ts_results_sentiment.items() if 'lstm' in v],
+                    directory='{directory}'.format(directory=directory),
+                    filename='mse_overall_lstm_sentiment.png',
+                    rotation=90
+                )
 
     #
-    # preprocess: left join on twitter dataset(s).
+    # timeseries analysis: overall stock index/volume
     #
-    for i,sn in enumerate(screen_name):
-        # merge with consistent date format
-        data[sn]['created_at'] = [datetime.strptime(
-            x.split()[0],
-            '%Y-%m-%d'
-        ) for x in data[sn]['created_at']]
-
-        # convert to string
-        data[sn]['created_at'] = data[sn]['created_at'].astype(str)
-
-        #
-        # some screen_name text multiple times a day, yet quandl only provides
-        #     daily prices.
-        #
-        data[sn] = data[sn].groupby(g).agg({
-            classify_index: lambda a: ''.join(map(str, a))
-        }).reset_index()
-
-        data[sn] = data[sn].set_index('created_at').join(
-            df_quandl.set_index('date'),
-            how='left'
-        ).reset_index()
-
-        #
-        # granger causality
-        #
-        # Note: this requires the above quandl join.
-        #
-        if analysis_granger:
-            for sentiment in sentiments:
-                if all(x in data[sn] for x in [ts_index, sentiment]):
-                    granger(
-                        data[sn][[ts_index, sentiment]],
-                        maxlag=3,
-                        directory='{directory}/{sn}/granger'.format(
-                            directory=directory,
-                            sn=sn
-                        ),
-                        suffix=sentiment
-                    )
-
-        # column names: used below
-        col_names = data[sn].columns.tolist()
-
-        #
-        # merge days (weekend, holidays) with no ticker value to previous day.
-        #
-        drop_indices = []
-        for i,(idx,row) in enumerate(data[sn].iterrows()):
-            if (i == 0 and pd.isnull(data[sn][ts_index][i])):
-                drop_indices.append(i)
-
-            elif (i > 0 and pd.isnull(data[sn][ts_index][i])):
-                if not pd.isnull(data[sn][ts_index][i-1]):
-                    for x in col_names:
-                        if x == classify_index:
-                            data[sn][classify_index][i] = '{previous} {current}'.format(
-                                previous=data[sn][classify_index][i-1],
-                                current=data[sn][classify_index][i]
-                            )
-                        else:
-                            data[sn][x][i] = data[sn][x][i-1]
-
-                    drop_indices.append(i-1)
-
-        #
-        # drop rows: rows with no tickers and empty classify_index.
-        #
-        drop_indices.extend(data[sn][data[sn][classify_index] == ''].index)
-        data[sn] = data[sn].drop(data[sn].index[drop_indices]).reset_index()
-
-        #
-        # index data: conditionally use z-score threshold to relabel index.
-        #
-        threshold = [0.5]
-        signals = peak_detection(
-            data=data[sn][ts_index],
-            threshold=threshold,
-            directory='{a}/{b}'.format(a=directory, b=sn),
-            suffix=sn
-        )
-
-        #
-        # case 1: z-score threshold determines trend index
-        #
-        if signals:
-            signal_result = []
-            for z in range(1, len(signals) + 1):
-                signal = signals[z-1]
-                for i,s in enumerate(signal):
-                    if (len(signal_result) == 0 or len(signal_result) == i):
-                        if s < 0:
-                            signal_result.append(-z)
-                        elif s > 0:
-                            signal_result.append(z)
-                        else:
-                            signal_result.append(0)
-
-                    elif (
-                        i < len(signal_result) and
-                        s < 0 and
-                        s < signal_result[i]
-                    ):
-                        signal_result[i] = -z
-
-                    elif (
-                        i < len(signal_result) and
-                        s > 0 and
-                        s > signal_result[i]
-                    ):
-                        signal_result[i] = z
-
-                    elif (
-                        i < len(signal_result) and
-                        s == 0 and
-                        s > signal_result[i]
-                    ):
-                        signal_result[i] = 0
-
-                    else:
-                        print('Error ({f}): {m}.'.format(
-                            f=this_file,
-                            m='distorted signal_result shape'
-                        ))
-
-            # monotic: if all same values use non z-score.
-            first = signal_result[0]
-            if all(x == first for x in signal_result):
-                data[sn]['trend'] = [0
-                    if data[sn][ts_index][i] > data[sn][ts_index].get(i-1, 0)
-                    else 1
-                    for i,x in enumerate(data[sn][ts_index])]
-
-            # not monotonic
-            else:
-                data[sn]['trend'] = signal_result
-
-        #
-        # case 2: previous index value determines trend index
-        #
-        else:
-            data[sn]['trend'] = [0
-                if data[sn][ts_index][i] > data[sn][ts_index].get(i-1, 0)
-                else 1
-                for i,x in enumerate(data[sn][ts_index])]
-
+    if analysis_ts:
         #
         # timeseries analysis: if dataset not 50 elements or more (x), use the
         #     default (p,q,d) range. Otherwise, the grid search (p,q,d) range
@@ -313,96 +202,26 @@ def analyze(
         # Note: if arima does not converge, or the adf test does not satisfy
         #       p < 0.05, the corresponding model is thrown out.
         #
-        if analysis_ts:
-            ts_stock = Timeseries(
-                df=data[sn],
-                normalize_key=ts_index,
-                date_index='created_at',
-                directory='{directory}/{sn}'.format(directory=directory, sn=sn),
-                suffix=ts_index,
-                lstm_epochs=1500,
-                lstm_dropout=0,
-                auto_scale=(50, 0.15)
-            )
-            ts_results[sn] = ts_stock.get_model_scores()
+        ts_stock = Timeseries(
+            df=joined_data[sn],
+            normalize_key=ts_index,
+            date_index='created_at',
+            directory='{directory}/{sn}'.format(directory=directory, sn=sn),
+            suffix=ts_index,
+            lstm_epochs=1500,
+            lstm_dropout=0,
+            auto_scale=(50, 0.15)
+        )
+        ts_results[sn] = ts_stock.get_model_scores()
 
-            if 'arima' in ts_results[sn]:
-                with open('{directory}/adf_{sn}_{type}.txt'.format(
-                    directory=directory_report,
-                    sn=sn,
-                    type=ts_index
-                ), 'w') as fp:
-                    print(ts_results[sn]['arima']['adf'], file=fp)
+        if 'arima' in ts_results[sn]:
+            with open('{directory}/adf_{sn}_{type}.txt'.format(
+                directory=directory_report,
+                sn=sn,
+                type=ts_index
+            ), 'w') as fp:
+                print(ts_results[sn]['arima']['adf'], file=fp)
 
-        #
-        # outlier class: remove class if training distribution is less than
-        #     50%, or greater than 150% of all other class distribution(s).
-        #
-        counter = defaultdict(lambda :0)
-        for k in data[sn]['trend']:
-            counter[k] += 1
-
-        if len(counter) > 2:
-            for key, val in counter.items():
-                if all(val < 0.5 * v for k,v in counter.items() if v != val):
-                    data[sn].drop(
-                        data[sn][data[sn]['trend'] == key].index.values,
-                        inplace=True
-                    )
-                    break
-
-                elif all(val > 1.5 * v for k,v in counter.items() if v != val):
-                    data[sn].drop(
-                        data[sn][data[sn]['trend'] == key].index,values,
-                        inplace=True
-                    )
-                    break
-
-        #
-        # sufficient data: analysis performed if adequate amount of data.
-        #
-        # Note: 3 classes are utilized as a base case, while an outlier class
-        #       can reduce the default to 2 classes. Each additional threshold
-        #       adds two additional classes.
-        #
-        if data[sn].shape[0] > (3 + ((len(threshold) - 1) * 2)) * 20:
-            #
-            # classify
-            #
-            if analysis_classify:
-                if all(x in data[sn] for x in ['trend', classify_index]):
-                    classify_results[sn] = classify(
-                        data[sn],
-                        key_class='trend',
-                        key_text=classify_index,
-                        directory='{directory}/{sn}'.format(
-                            directory=directory,
-                            sn=sn
-                        ),
-                        top_words=25,
-                        stopwords=stopwords,
-                        k=750
-                    )
-
-    #
-    # ensembled scores
-    #
-    if analysis_classify:
-        if any(
-            pd.notnull(k) and
-            pd.notnull(v) and
-            isinstance(v, tuple) for k,v in classify_results.items()
-        ):
-            plot_bar(
-                labels=[k for k,v in classify_results.items() if pd.notnull(k)],
-                performance=[v[0] for k,v in classify_results.items()
-                    if pd.notnull(v) and isinstance(v, tuple)],
-                directory='{directory}'.format(directory=directory),
-                filename='accuracy_overall.png',
-                rotation=90
-            )
-
-    if analysis_ts:
         if any(
             pd.notnull(k) and
             pd.notnull(v) and
@@ -431,3 +250,66 @@ def analyze(
                 rotation=90
             )
 
+    #
+    # classification analysis: twitter corpus (X), with stock index (y)
+    #
+    if analysis_classify:
+        #
+        # outlier class: remove class if training distribution is less than
+        #     50%, or greater than 150% of all other class distribution(s).
+        #
+        counter = defaultdict(lambda :0)
+        for k in joined_data[sn]['trend']:
+            counter[k] += 1
+
+        if len(counter) > 2:
+            for key, val in counter.items():
+                if all(val < 0.5 * v for k,v in counter.items() if v != val):
+                    joined_data[sn].drop(
+                        joined_data[sn][joined_data[sn]['trend'] == key].index.values,
+                        inplace=True
+                    )
+                    break
+
+                elif all(val > 1.5 * v for k,v in counter.items() if v != val):
+                    joined_data[sn].drop(
+                        joined_data[sn][joined_data[sn]['trend'] == key].index,values,
+                        inplace=True
+                    )
+                    break
+
+        #
+        # sufficient data: analysis performed if adequate amount of data.
+        #
+        # Note: 3 classes are utilized as a base case, while an outlier class
+        #       can reduce the default to 2 classes. Each additional threshold
+        #       adds two additional classes.
+        #
+        if joined_data[sn].shape[0] > (3 + ((len(threshold) - 1) * 2)) * 20:
+            if all(x in joined_data[sn] for x in ['trend', classify_index]):
+                classify_results[sn] = classify(
+                    joined_data[sn],
+                    key_class='trend',
+                    key_text=classify_index,
+                    directory='{directory}/{sn}'.format(
+                        directory=directory,
+                        sn=sn
+                    ),
+                    top_words=25,
+                    stopwords=stopwords,
+                    k=750
+                )
+
+        if any(
+            pd.notnull(k) and
+            pd.notnull(v) and
+            isinstance(v, tuple) for k,v in classify_results.items()
+        ):
+            plot_bar(
+                labels=[k for k,v in classify_results.items() if pd.notnull(k)],
+                performance=[v[0] for k,v in classify_results.items()
+                    if pd.notnull(v) and isinstance(v, tuple)],
+                directory='{directory}'.format(directory=directory),
+                filename='accuracy_overall.png',
+                rotation=90
+            )
---
+++ app/join_data.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python
+
+import os
+import pandas as pd
+from datetime import datetime
+from brain.controller.peak_detection import peak_detection
+
+
+def join_data(
+    data,
+    df_quandl,
+    screen_name,
+    directory='viz',
+    directory_report='reports',
+    sentiments = ['negative', 'neutral', 'positive'],
+    classify_index='full_text',
+    ts_index='value'
+):
+    '''
+
+    initialize twitter and quandl dataset(s).
+
+    '''
+
+    classify_results = {}
+    ts_results = {}
+    ts_results_sentiment = {}
+    g = ['created_at', 'screen_name'] + sentiments
+    this_file = os.path.basename(__file__)
+
+    #
+    # preprocess: left join on twitter dataset(s).
+    #
+    for i,sn in enumerate(screen_name):
+        # merge with consistent date format
+        data[sn]['created_at'] = [datetime.strptime(
+            x.split()[0],
+            '%Y-%m-%d'
+        ) for x in data[sn]['created_at']]
+
+        # convert to string
+        data[sn]['created_at'] = data[sn]['created_at'].astype(str)
+
+        #
+        # some screen_name text multiple times a day, yet quandl only provides
+        #     daily prices.
+        #
+        data[sn] = data[sn].groupby(g).agg({
+            classify_index: lambda a: ''.join(map(str, a))
+        }).reset_index()
+
+        if 'created_at' in data[sn]:
+            data[sn].set_index('created_at', inplace=True)
+
+        if 'date' in df_quandl:
+            df_quandl.set_index('date', inplace=True)
+
+        print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa')
+        print(df_quandl.index.name)
+        print(list(df_quandl))
+        print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAaa')
+
+        data[sn] = data[sn].join(df_quandl, how='left')
+        data[sn].index.name = 'created_at'
+
+        # column names: used below
+        col_names = data[sn].columns.tolist()
+
+        #
+        # merge days (weekend, holidays) with no ticker value to previous day.
+        #
+        drop_indices = []
+        for i,(idx,row) in enumerate(data[sn].iterrows()):
+            if (i == 0 and pd.isnull(data[sn][ts_index].values[i])):
+                drop_indices.append(i)
+
+            elif (i > 0 and pd.isnull(data[sn][ts_index].values[i])):
+                if not pd.isnull(data[sn][ts_index].values[i-1]):
+                    for x in col_names:
+                        if x == classify_index:
+                            data[sn][classify_index].replace(
+                                i,
+                                '{previous} {current}'.format(
+                                    previous=data[sn][classify_index].values[i-1],
+                                    current=data[sn][classify_index].values[i]
+                                )
+                            )
+                        else:
+                            data[sn][x].replace(i, data[sn][x].values[i-1])
+
+                    drop_indices.append(i-1)
+
+        #
+        # drop rows: rows with no tickers and empty classify_index.
+        #
+        drop_indices.extend(data[sn][data[sn][classify_index] == ''].index)
+        data[sn] = data[sn].drop(data[sn].index[drop_indices]).reset_index()
+
+        #
+        # index data: conditionally use z-score threshold to relabel index.
+        #
+        threshold = [0.5]
+        signals = peak_detection(
+            data=data[sn][ts_index],
+            threshold=threshold,
+            directory='{a}/{b}'.format(a=directory, b=sn),
+            suffix=sn
+        )
+
+        #
+        # case 1: z-score threshold determines trend index
+        #
+        if signals:
+            signal_result = []
+            for z in range(1, len(signals) + 1):
+                signal = signals[z-1]
+                for i,s in enumerate(signal):
+                    if (len(signal_result) == 0 or len(signal_result) == i):
+                        if s < 0:
+                            signal_result.append(-z)
+                        elif s > 0:
+                            signal_result.append(z)
+                        else:
+                            signal_result.append(0)
+
+                    elif (
+                        i < len(signal_result) and
+                        s < 0 and
+                        s < signal_result[i]
+                    ):
+                        signal_result[i] = -z
+
+                    elif (
+                        i < len(signal_result) and
+                        s > 0 and
+                        s > signal_result[i]
+                    ):
+                        signal_result[i] = z
+
+                    elif (
+                        i < len(signal_result) and
+                        s == 0 and
+                        s > signal_result[i]
+                    ):
+                        signal_result[i] = 0
+
+                    else:
+                        print('Error ({f}): {m}.'.format(
+                            f=this_file,
+                            m='distorted signal_result shape'
+                        ))
+
+            # monotic: if all same values use non z-score.
+            first = signal_result[0]
+            if all(x == first for x in signal_result):
+                data[sn]['trend'] = [0
+                    if data[sn][ts_index].values[i] > data[sn][ts_index].get(i-1, 0)
+                    else 1
+                    for i,x in enumerate(data[sn][ts_index])]
+
+            # not monotonic
+            else:
+                data[sn]['trend'] = signal_result
+
+        #
+        # case 2: previous index value determines trend index
+        #
+        else:
+            data[sn]['trend'] = [0
+                if data[sn][ts_index].values[i] > data[sn][ts_index].get(i-1, 0)
+                else 1
+                for i,x in enumerate(data[sn][ts_index])]
+
+    return(data)
