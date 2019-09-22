2cab0ed609e51b13357213cf35bfbac80465a4be
jeffrey levesque <jeff1evesque@yahoo.com>
2019-09-22 09:06:25 -0400

    #100: refactor codebase

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ from datetime import date, datetime
 import dateutil.relativedelta
 from consumer.twitter import tweet_sn
 from consumer.quandl import quandl
-from app.analysis import analyze
 from app.exploratory import explore
+from app.join_data import join_data
+from app.analysis import analyze, analyze_ts
+from app.create_directory import create_directory
 from brain.utility.stopwords import stopwords, stopwords_topics
 
 #
@@ -31,9 +33,9 @@ screen_name = [
     'SJosephBurns'
 ]
 codes = [
-##    ('BATS', 'BATS_AAPL'),
+    ('BATS', 'BATS_AAPL'),
 ##    ('BATS', 'BATS_AMZN'),
-    ('BATS', 'BATS_GOOGL'),
+##    ('BATS', 'BATS_GOOGL'),
 ##    ('BATS', 'BATS_MMT'),
 ##    ('BATS', 'BATS_NFLX'),
 ##    ('CHRIS', 'CBOE_VX1'),
@@ -47,9 +49,33 @@ codes = [
 ]
 end_date = date.today()
 start_date = end_date - dateutil.relativedelta.relativedelta(years=5)
+sentiments = ['negative', 'neutral', 'positive']
+classify_index = 'full_text'
+ts_index = 'value'
+
+arima_auto_scale = None
+lstm_epochs = 750
+classify_threshold = [0.5]
+classify_chi2 = 100
 
 stopwords.extend([x.lower() for x in screen_name])
 stopwords_topics.extend(stopwords)
+
+drop_cols = [
+    'compound',
+    'retweet_count',
+    'favorite_count',
+    'user_mentions',
+    'Short Volume'
+]
+
+#
+# create directories
+#
+create_directory(
+    screen_name,
+    directory_lstm='viz/lstm_{a}'.format(a=lstm_epochs)
+)
 
 #
 # harvest tweets
@@ -85,42 +111,67 @@ df_quandl = quandl(
 #
 # @arima_auto_scale, only applied to sentiment timeseries analysis.
 #
-arima_auto_scale=None
-lstm_epochs=750
-classify_threshold=[0.5]
-classify_chi2=100
-
 for x in df_quandl:
-    analyze(
+    #
+    # local variables
+    #
+    sub_directory = '{b}--{c}'.format(
+        b=x['database'].lower(),
+        c=x['dataset'].lower()
+    )
+
+    #
+    # join data: twitter and quandl
+    #
+    # Note: memory footprint reduced by removing unused columns.
+    #
+    df = join_data(
         data=data,
+        df_quandl=x['data'],
+        screen_name=screen_name,
+        drop_cols=drop_cols,
+        sentiments=sentiments,
+        classify_index=classify_index,
+        ts_index=ts_index
+    )[1]
+
+    #
+    # general analysis
+    #
+    analyze(
+        df=df,
         df_quandl=x['data'],
         arima_auto_scale=arima_auto_scale,
         lstm_epochs=lstm_epochs,
         classify_threshold=classify_threshold,
-        directory_granger='viz/granger/{b}--{c}'.format(
-            b=x['database'].lower(),
-            c=x['dataset'].lower()
-        ),
-        directory_lstm='viz/lstm_{a}/{b}--{c}'.format(
+        directory_granger='viz/granger/{a}'.format(a=sub_directory),
+        directory_lstm='viz/lstm_{a}/{b}'.format(
             a=lstm_epochs,
-            b=x['database'].lower(),
-            c=x['dataset'].lower()
+            b=sub_directory
         ),
-        directory_arima='viz/arima/{b}--{c}'.format(
-            b=x['database'].lower(),
-            c=x['dataset'].lower()
-        ),
-        directory_class='viz/classification/{b}--{c}'.format(
-            b=x['database'].lower(),
-            c=x['dataset'].lower()
-        ),
-        directory_report='reports/{b}'.format(
-            b=x['dataset']
-        ),
+        directory_arima='viz/arima/{a}'.format(a=sub_directory),
+        directory_class='viz/classification/{a}'.format(a=sub_directory),
+        directory_report='reports/{a}'.format(a=x['dataset']),
         screen_name=screen_name,
         stopwords=stopwords,
+        classify_index=classify_index,
+        ts_index=ts_index,
         analysis_granger=False,
-        analysis_ts=True,
-        analysis_ts_sentiment=False,
+        analysis_ts=False,
         analysis_classify=False
     )
+
+#
+# sentiment timeseries: analysis on twitter corpus for each financial analyst.
+#
+# Note: stock data is always a superset of twitter corpus. This is why only
+#       the last instance of the joined df dataframe is needed.
+#
+analyze_ts(
+    df,
+    screen_name,
+    arima_auto_scale=arima_auto_scale,
+    lstm_epochs=lstm_epochs,
+    directory_lstm='viz/lstm_{a}'.format(a=lstm_epochs),
+    directory_arima='viz/arima'
+)
--- app/analysis.py
+++ app/analysis.py
@@ -10,11 +10,10 @@ from brain.controller.classifier import classify
 from brain.controller.timeseries import Timeseries
 from brain.controller.granger import granger
 from brain.controller.peak_detection import peak_detection
-from app.join_data import join_data
 
 
 def analyze(
-    data,
+    df,
     df_quandl,
     screen_name,
     stopwords=[],
@@ -32,67 +31,23 @@ def analyze(
     ts_index='value',
     analysis_granger=True,
     analysis_ts=True,
-    analysis_ts_sentiment=True,
     analysis_classify=True
 ):
     '''
 
-    general analysis on provided screen names.
+    general analysis on provided screen names:
+
+        - granger: normalized sentiment vs stock
+        - lstm: stock index/volume
+        - arima: stock index/volume
+        - classification: sentiment with classes defined by signal analysis
 
     '''
 
     classify_results = {}
     ts_results = {}
-    ts_results_sentiment = {}
     g = ['created_at', 'screen_name'] + sentiments
-    drop_cols = [
-        'compound',
-        'retweet_count',
-        'favorite_count',
-        'user_mentions',
-        'Short Volume'
-    ]
     this_file = os.path.basename(__file__)
-    directories = [
-        directory_report,
-        '{a}/stock'.format(a=directory_lstm),
-        '{a}/stock'.format(a=directory_arima)
-    ]
-    directories_sn = [
-        directory_granger,
-        directory_class,
-        directory_lstm,
-        directory_arima
-    ]
-
-    #
-    # create directories
-    #
-    for d in directories:
-        full_path = d.split('/')
-        if not os.path.exists(os.path.join(*full_path)):
-            os.makedirs(os.path.join(*full_path))
-
-    for d in directories_sn:
-        for sn in screen_name:
-            full_path = '{d}/{sn}'.format(d=d, sn=sn).split('/')
-            if not os.path.exists(os.path.join(*full_path)):
-                os.makedirs(os.path.join(*full_path))
-
-    #
-    # join data: twitter and quandl
-    #
-    # Note: memory footprint reduced by removing unused columns.
-    #
-    joined_data, joined_data_agg = join_data(
-        data=data,
-        df_quandl=df_quandl,
-        screen_name=screen_name,
-        drop_cols=drop_cols,
-        sentiments=sentiments,
-        classify_index=classify_index,
-        ts_index=ts_index
-    )
 
     #
     # granger analysis:
@@ -100,7 +55,7 @@ def analyze(
     # Note: requires vader sentiment scores.
     #
     if analysis_granger:
-        initialized_data = joined_data_agg
+        initialized_data = df
 
         for i,sn in enumerate(screen_name):
             # merge with consistent date format
@@ -130,33 +85,172 @@ def analyze(
                     )
 
     #
-    # timeseries analysis: sentiment
+    # timeseries analysis: overall stock index/volume
     #
-    if analysis_ts_sentiment:
-        initialized_data = joined_data_agg
+    if analysis_ts:
+        #
+        # timeseries analysis: if dataset not 50 elements or more (x), use the
+        #     default (p,q,d) range. Otherwise, the grid search (p,q,d) range
+        #     is determined by 0.15x:
+        #
+        #     (p,q,d) = (range(0, 0.15x), range(0, 0.15x), range(0, 0.15x))
+        #
+        # Note: if arima does not converge, or the adf test does not satisfy
+        #       p < 0.05, the corresponding model is thrown out.
+        #
+        ts_stock = Timeseries(
+            df=df_quandl,
+            normalize_key=ts_index,
+            date_index='date',
+            directory_arima='{a}/sentiment'.format(directory_arima),
+            directory_lstm='{a}/sentiment'.format(directory_lstm),
+            suffix=ts_index,
+            arima_auto_scale=(50, 0.15),
+            lstm_epochs=lstm_epochs,
+            lstm_dropout=0
+        )
+        ts_results = ts_stock.get_model_scores()
 
-        for sn in screen_name:
-            ts_results_sentiment[sn] = {}
+        if 'arima' in ts_results:
+            plot_bar(
+                labels=['overall'],
+                performance=ts_results['arima']['mse'],
+                directory='{a}/sentiment'.format(directory_arima),
+                filename='mse_overall_arima.png',
+                rotation=90
+            )
+
+            with open('{d}/adf_{type}.txt'.format(
+                d=directory_report,
+                type=ts_index
+            ), 'w') as fp:
+                print(ts_results['arima']['adf'], file=fp)
+
+        if 'lstm' in ts_results:
+            plot_bar(
+                labels=['overall'],
+                performance=ts_results['lstm']['mse'],
+                directory='{a}/sentiment'.format(directory_lstm),
+                filename='mse_overall_lstm.png',
+                rotation=90
+            )
+
+    #
+    # classification analysis: twitter corpus (X), with stock index (y)
+    #
+    if analysis_classify:
+        for i,sn in enumerate(screen_name):
+            data = peak_detection(
+                data=df[sn],
+                ts_index=ts_index,
+                directory='{a}/{b}'.format(a=directory_class, b=sn),
+                threshold=classify_threshold
+            )
 
             #
-            # timeseries model on sentiment
+            # outlier class: remove class if training distribution is less than
+            #     50%, or greater than 150% of all other class distribution(s).
             #
-            for sent in sentiments:
-                ts_results_sentiment[sn][sent] = {}
+            counter = defaultdict(lambda :0)
+            for k in data['trend']:
+                counter[k] += 1
 
-                if (
-                    'created_at' == initialized_data[sn].index.name and
-                    sent in initialized_data[sn]
+            if len(counter) > 2:
+                for key, val in counter.items():
+                    if all(val > 1.5 * v for k,v in counter.items() if v != val):
+                        data.drop(
+                            data[data['trend'] == key].index.values[0],
+                            inplace=True
+                        )
+
+                for key, val in counter.items():
+                    if all(val < 0.5 * v for k,v in counter.items() if v != val):
+                        data.drop(
+                            data[data['trend'] == key].index.values,
+                            inplace=True
+                        )
+
+            #
+            # sufficient data: analysis performed if adequate amount of data.
+            #
+            # Note: 3 classes are utilized as a base case, while an outlier class
+            #       can reduce the default to 2 classes. Each additional threshold
+            #       adds two additional classes.
+            #
+            if data.shape[0] > (3 + ((len(classify_threshold) - 1) * 2)) * 20:
+                if all(x in data for x in ['trend', classify_index]):
+                    #
+                    # target vector: requires a minimum of 2 unique classes to
+                    #     train a classifier.
+                    #
+                    if len(data['trend'].unique().tolist()) > 1:
+                        classify_results[sn] = classify(
+                            data,
+                            key_class='trend',
+                            key_text=classify_index,
+                            directory='{d}/{sn}'.format(
+                                d=directory_class,
+                                sn=sn
+                            ),
+                            top_words=25,
+                            stopwords=stopwords,
+                            k=classify_chi2
+                        )
+
+            if any(
+                pd.notnull(k) and
+                pd.notnull(v) and
+                isinstance(v, tuple) for k,v in classify_results.items()
+            ):
+                plot_bar(
+                    labels=[k for k,v in classify_results.items() if pd.notnull(k)],
+                    performance=[v[0] for k,v in classify_results.items()
+                        if pd.notnull(v) and isinstance(v, tuple)],
+                    directory=directory_class,
+                    filename='accuracy_overall.png',
+                    rotation=90
+                )
+
+def analyze_ts(
+    df,
+    screen_name,
+    sentiments=['negative', 'neutral', 'positive'],
+    arima_auto_scale=None,
+    lstm_epochs=750,
+    directory_lstm='viz/lstm',
+    directory_arima='viz/arima',
+    directory_report='reports'
+):
+    '''
+
+    timeseries analysis using arima, and lstm on sentiment scores.
+
+    '''
+
+    ts_results_sentiment = {}
+
+    for sn in screen_name:
+        ts_results_sentiment[sn] = {}
+
+        #
+        # timeseries model on sentiment
+        #
+        for sent in sentiments:
+            ts_results_sentiment[sn][sent] = {}
+
+            if (
+                'created_at' == df[sn].index.name and
+                    sent in df[sn]
                 ):
                     ts_sentiment = Timeseries(
-                        df=initialized_data[sn],
+                        df=df[sn],
                         normalize_key=sent,
                         date_index=None,
-                        directory_arima='{d}/{sn}'.format(
+                        directory_arima='{d}/sentiment/{sn}'.format(
                             d=directory_arima,
                             sn=sn
                         ),
-                        directory_lstm='{d}/{sn}'.format(
+                        directory_lstm='{d}/sentiment/{sn}'.format(
                             d=directory_lstm,
                             sn=sn
                         ),
@@ -234,131 +328,4 @@ def analyze(
                     directory=directory_lstm,
                     filename='mse_overall_lstm_{sent}.png'.format(sent=sent),
                     rotation=60
-                )
-
-    #
-    # timeseries analysis: overall stock index/volume
-    #
-    if analysis_ts:
-        #
-        # timeseries analysis: if dataset not 50 elements or more (x), use the
-        #     default (p,q,d) range. Otherwise, the grid search (p,q,d) range
-        #     is determined by 0.15x:
-        #
-        #     (p,q,d) = (range(0, 0.15x), range(0, 0.15x), range(0, 0.15x))
-        #
-        # Note: if arima does not converge, or the adf test does not satisfy
-        #       p < 0.05, the corresponding model is thrown out.
-        #
-        ts_stock = Timeseries(
-            df=df_quandl,
-            normalize_key=ts_index,
-            date_index='date',
-            directory_arima='{a}/sentiment'.format(directory_arima),
-            directory_lstm='{a}/sentiment'.format(directory_lstm),
-            suffix=ts_index,
-            arima_auto_scale=(50, 0.15),
-            lstm_epochs=lstm_epochs,
-            lstm_dropout=0
-        )
-        ts_results = ts_stock.get_model_scores()
-
-        if 'arima' in ts_results:
-            plot_bar(
-                labels=['overall'],
-                performance=ts_results['arima']['mse'],
-                directory='{a}/sentiment'.format(directory_arima),
-                filename='mse_overall_arima.png',
-                rotation=90
-            )
-
-            with open('{d}/adf_{type}.txt'.format(
-                d=directory_report,
-                type=ts_index
-            ), 'w') as fp:
-                print(ts_results['arima']['adf'], file=fp)
-
-        if 'lstm' in ts_results:
-            plot_bar(
-                labels=['overall'],
-                performance=ts_results['lstm']['mse'],
-                directory='{a}/sentiment'.format(directory_lstm),
-                filename='mse_overall_lstm.png',
-                rotation=90
-            )
-
-    #
-    # classification analysis: twitter corpus (X), with stock index (y)
-    #
-    if analysis_classify:
-        for i,sn in enumerate(screen_name):
-            data = peak_detection(
-                data=joined_data_agg[sn],
-                ts_index=ts_index,
-                directory='{a}/{b}'.format(a=directory_class, b=sn),
-                threshold=classify_threshold
-            )
-
-            #
-            # outlier class: remove class if training distribution is less than
-            #     50%, or greater than 150% of all other class distribution(s).
-            #
-            counter = defaultdict(lambda :0)
-            for k in data['trend']:
-                counter[k] += 1
-
-            if len(counter) > 2:
-                for key, val in counter.items():
-                    if all(val > 1.5 * v for k,v in counter.items() if v != val):
-                        data.drop(
-                            data[data['trend'] == key].index.values[0],
-                            inplace=True
-                        )
-
-                for key, val in counter.items():
-                    if all(val < 0.5 * v for k,v in counter.items() if v != val):
-                        data.drop(
-                            data[data['trend'] == key].index.values,
-                            inplace=True
-                        )
-
-            #
-            # sufficient data: analysis performed if adequate amount of data.
-            #
-            # Note: 3 classes are utilized as a base case, while an outlier class
-            #       can reduce the default to 2 classes. Each additional threshold
-            #       adds two additional classes.
-            #
-            if data.shape[0] > (3 + ((len(classify_threshold) - 1) * 2)) * 20:
-                if all(x in data for x in ['trend', classify_index]):
-                    #
-                    # target vector: requires a minimum of 2 unique classes to
-                    #     train a classifier.
-                    #
-                    if len(data['trend'].unique().tolist()) > 1:
-                        classify_results[sn] = classify(
-                            data,
-                            key_class='trend',
-                            key_text=classify_index,
-                            directory='{d}/{sn}'.format(
-                                d=directory_class,
-                                sn=sn
-                            ),
-                            top_words=25,
-                            stopwords=stopwords,
-                            k=classify_chi2
-                        )
-
-            if any(
-                pd.notnull(k) and
-                pd.notnull(v) and
-                isinstance(v, tuple) for k,v in classify_results.items()
-            ):
-                plot_bar(
-                    labels=[k for k,v in classify_results.items() if pd.notnull(k)],
-                    performance=[v[0] for k,v in classify_results.items()
-                        if pd.notnull(v) and isinstance(v, tuple)],
-                    directory=directory_class,
-                    filename='accuracy_overall.png',
-                    rotation=90
                 )
